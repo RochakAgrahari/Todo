d9562dc2a92dc45be2d3e5100be5625ae91b6b4a
RochakAgrahari <142715000+RochakAgrahari@users.noreply.github.com>
2025-04-01 01:49:21 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useTodo } from "../contexts/TodoContext.js";
+import { useTodo } from "../contexts/TodoContext";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchWeather, clearWeatherError } from "../slices/weatherSlice.jsx";
+import { fetchWeather, clearWeatherError } from "../slices/weatherSlice";
 
 function TodoItem({ todo }) {
   const [isEditable, setIsEditable] = useState(false);
